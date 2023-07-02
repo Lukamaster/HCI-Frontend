b731eb729ea7e265024d5fe6c6a237891eb03610
Lukamaster <luka.ceslarov@protonmail.com>
2023-07-02 20:54:10 +0000

    Fixed axios baseURL to route to correct address on webserver for reverse-proxy

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://back-end:8080/api",
+    baseURL: "http://16.170.251.252/api",
     headers: {
         'Access-Control-Allow-Origin' : '*'
     }
